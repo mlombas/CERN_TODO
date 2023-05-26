bd80ca187ebda30b2eb4a9e2c4b4457974e73748
mlombasc <mariolombasc@gmail.com>
2023-05-26 12:48:16 +0200

    Added methods to add and get task

---
+++ src/main/java/ch/cern/todo/configurations/Services.java
@@ -0,0 +1,2 @@
+package ch.cern.todo.configurations;public class Services {
+}
---
+++ src/main/java/ch/cern/todo/controllers/POJO/NewTaskPOJO.java
@@ -0,0 +1,14 @@
+package ch.cern.todo.controllers.POJO;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Value;
+
+import java.util.Date;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Value
+public class NewTaskPOJO {
+    private String name;
+    private String description;
+    private Date deadline;
+}
